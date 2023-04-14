4143376b3b69a1d5c098e86c8a9f7550aea9cdc7
taufik0747 <rahamantaufik1999@gmail.com>
2023-04-14 18:41:05 +0000

    Successfully submitted code for ME_DSA_BASIC_SET_MAP_MODULE_MOSTFREQUENT

---
+++ ME_DSA_BASIC_SET_MAP/QUESTION_MostFrequent.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class MostFrequent {
+    // NOTE: Please do not modify this function
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        Object[] ans = mostFrequent(s);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+
+    // TODO: Implement this method
+    static Object[] mostFrequent(String s) {
+        Map<Character, Integer> map= new HashMap<>();
+            for(int i=0;i<s.length();i++){
+                char c=s.charAt(i);
+                if(!map.containsKey(c)){
+                    map.put(c,1);
+                }
+                    else{
+                        int currentValue=map.get(c);
+                        map.put(c, currentValue+1);
+                    }
+                }
+                int maxCount=0;
+                char maxChar='a';
+                for(Map.Entry<Character, Integer> entry:map.entrySet()){
+                   if(entry.getValue()>maxCount){
+                       maxCount=entry.getValue();
+                        maxChar=entry.getKey();
+
+                        }
+                        else if(entry.getValue()==maxCount){
+                            if(entry.getKey()<maxChar){
+                                maxChar=entry.getKey();
+                            }
+                        }
+                    }
+                
+                return new Object[]{maxChar,maxCount};
+            } 
+    }
+
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

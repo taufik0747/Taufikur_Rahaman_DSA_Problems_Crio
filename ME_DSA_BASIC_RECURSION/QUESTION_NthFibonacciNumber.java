464db2e2a2c3a26581359d16bd097d21946e20d4
taufik0747 <rahamantaufik1999@gmail.com>
2023-04-16 18:15:45 +0000

    Successfully submitted code for ME_DSA_BASIC_RECURSION_MODULE_NTHFIBONACCINUMBER

---
+++ ME_DSA_BASIC_RECURSION/QUESTION_NthFibonacciNumber.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+
+class NthFibonacciNumber{
+    // NOTE: Please do not modify this function
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = nthFibonacciNumber(n);
+        System.out.println(ans);
+    }
+
+    // TODO: Implement this method
+    static int nthFibonacciNumber( int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        else{
+            return nthFibonacciNumber(n-1)+nthFibonacciNumber(n-2);
+        }
+    }
+}
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

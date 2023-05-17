6c0088f4407a0c4e60a2b656aadd75a05601f79c
taufik0747 <rahamantaufik1999@gmail.com>
2023-05-17 19:57:01 +0000

    Successfully submitted code for ME_DSA_ARRAY_SORTING_MODULE_NUMSORT

---
+++ ME_DSA_ARRAY_SORTING/QUESTION_NumSort.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class NumSort {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String arr[] = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        String sortedArray[] = numSort(n, arr);
+
+        for (String num : sortedArray) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    static String[] numSort(int n, String[] arr) {
+        Arrays.sort(arr, (left, right) ->
+        {
+            /* If length of left != right, then return
+               the diff of the length else  use compareTo
+               function to compare values.*/
+            if (left.length() != right.length())
+                return left.length() - right.length();
+             return left.compareTo(right);
+        });
+        return arr;
+    }
+    }
+
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

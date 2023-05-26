7cae0a615ca5242f9055d4dcf328e0c44911366d
taufik0747 <rahamantaufik1999@gmail.com>
2023-05-26 15:35:46 +0530

    Successfully submitted code for ME_PS_DS_PLAYGROUND_MODULE_REMOVEDUPLICATESFROMSORTEDARRAY

---
+++ ME_PS_DS_PLAYGROUND/QUESTION_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class RemoveDuplicatesFromSortedArray {
+    
+    // Implement your solution by completing the below function
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int j = 0; // Pointer to track the position of updated elements
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        
+        return j + 1; // Return the modified array length
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[scanner.nextInt()];
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = scanner.nextInt();
+        scanner.close();
+
+        int result = new RemoveDuplicatesFromSortedArray().removeDuplicates(numbers);
+        System.out.print(Integer.toString(result));
+    }
+}
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

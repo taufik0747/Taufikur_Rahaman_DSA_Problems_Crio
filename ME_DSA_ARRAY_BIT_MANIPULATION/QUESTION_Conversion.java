f66100d8bad05bee9d0e58d8b351efbc1959cb68
taufik0747 <rahamantaufik1999@gmail.com>
2023-05-26 11:27:25 +0000

    Successfully submitted code for ME_DSA_ARRAY_BIT_MANIPULATION_MODULE_CONVERSION

---
+++ ME_DSA_ARRAY_BIT_MANIPULATION/QUESTION_Conversion.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Conversion{
+     
+	public static int conversion(long A,long B)
+	{
+              long a_xor_b = A ^ B;  // Step 1
+    return numberOfOneBits(a_xor_b);  // Step 2
+
+	}
+      static int numberOfOneBits(long n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);  // Flips the least significant 1-bit to 0
+            count++;
+        }
+        return count;
+    }
+
+	public static void main(String [] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		long a=sc.nextLong();
+		long b=sc.nextLong();
+		System.out.println(conversion(a,b));
+	}	
+}
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

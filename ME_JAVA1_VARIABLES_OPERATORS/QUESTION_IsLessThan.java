0afa19f209b09f410069009b901166b418da304d
taufik0747 <rahamantaufik1999@gmail.com>
2023-06-06 15:37:25 +0530

    Successfully submitted code for ME_JAVA1_VARIABLES_OPERATORS_MODULE_ISLESSTHAN

---
+++ ME_JAVA1_VARIABLES_OPERATORS/QUESTION_IsLessThan.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+  public static boolean isLessThan(int a,int b){
+    return (a<b);
+  } 
+
+
+  public static void main(String args[]) {
+        assert (isLessThan(1, 9) == true) : "Expect true for a = 1, b = 9";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
+

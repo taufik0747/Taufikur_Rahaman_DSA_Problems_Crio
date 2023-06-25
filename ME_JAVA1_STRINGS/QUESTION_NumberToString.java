6e2b2debebd5d59f5fc5b9f52392d89fb1b0d65e
taufik0747 <rahamantaufik1999@gmail.com>
2023-06-25 21:35:55 +0530

    Successfully submitted code for ME_JAVA1_STRINGS_MODULE_NUMBERTOSTRING

---
+++ ME_JAVA1_STRINGS/QUESTION_NumberToString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static String numberToString(int num) {
+      return String.valueOf(num);
+    }
+
+    public static void main(String args[]) {
+
+      assert (numberToString(11).equals("11")) : "Expect \"11\" when num=11";
+      System.out.println("All test cases in main function passed");
+
+    }
+}
+

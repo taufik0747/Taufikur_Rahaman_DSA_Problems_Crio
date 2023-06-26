7a349be4abf86e18e946d5eab47b908f672819d9
taufik0747 <rahamantaufik1999@gmail.com>
2023-06-26 16:35:35 +0530

    Successfully submitted code for ME_JAVA1_COLLECTIONS_MODULE_IDENTICALGOLDBARS

---
+++ ME_JAVA1_COLLECTIONS/QUESTION_IdenticalGoldBars.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// import static org.junit.Assert.assertEquals;
+
+public class Solution {
+
+    public static boolean isIdentical(Float s1, Float s2) {
+        return s1.equals(s2);
+    }
+
+    public static void main(String args[]) {
+        Float s1 = 2.56789f;
+        Float s2 = 2.56789f;
+        assert isIdentical(s1, s2) == true : "Expect true for s1 = 2.56789 and s2 = 2.56789";
+        s1 = 1.7689f;
+        s2 = 3.2456f;
+        assert isIdentical(s1, s2) == false : "Expect false for s1 = 1.7689 and s2 = 3.2456";
+        System.out.println("All test cases in main function passed");
+    }
+}
+

2e1f5adb9db894392768f426a26eeb5981b66b68
taufik0747 <rahamantaufik1999@gmail.com>
2023-06-26 16:35:51 +0530

    Successfully submitted code for ME_JAVA1_COLLECTIONS_MODULE_INTERNALMARKS

---
+++ ME_JAVA1_COLLECTIONS/QUESTION_InternalMarks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+// import static org.junit.Assert.assertEquals;
+
+public class Solution {
+
+    public static List<Integer> updateMarks(List<Integer> list, int rollNo, int marks) {
+         list.set(rollNo, marks); // Replace the element at the given roll number with the correct marks
+        return list;
+    }
+
+    public static void main(String args[]) {
+        List<Integer> list = Arrays.asList(12, 35, 67, 89, 9);
+        assert list.equals(updateMarks(list, 4, 98)) : "Expect [12, 35, 67, 89, 98] after updating rollNo = 4";
+        System.out.println("All test cases in main function passed");
+    }
+}
+

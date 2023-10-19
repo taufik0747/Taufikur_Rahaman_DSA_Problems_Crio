4d99bf5f8aa005f141a294e3e8501dd0bdec90fb
taufik0747 <rahamantaufik1999@gmail.com>
2023-10-19 19:04:41 +0000

    Successfully submitted code for ME_PS_DS_CONCEPT_LINKED_LIST_NEW_MODULE_SORTLIST

---
+++ ME_PS_DS_CONCEPT_LINKED_LIST_NEW/QUESTION_SortList.java
@@ -0,0 +1,100 @@
+import crio.ds.List.ListNode;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+class Solution {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; // Base case: List is empty or has only one element.
+        }
+
+        // Split the linked list into two halves.
+        ListNode mid = findMiddle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        // Recursively sort both halves.
+        left = sortList(left);
+        right = sortList(right);
+
+        // Merge the sorted halves back together.
+        return merge(left, right);
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output
+
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving and see if you can recollect solving similar problems in the past
+      a. Obvious logic (this would only test ability to convert logic to code)
+      b. Figuring out logic
+      c. Knowledge of specific algorithm, data structure or pattern
+      d. Knowledge of specific domain or concepts
+      e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one based on the TC/SC requirements
+3. Get to a point where you can explain your approach to a 10 year old
+
+Milestone 3 : Come up with an Instruction Manual for a 10 year old
+1. Take a stab at the high-level logic & write it down like a detailed Instruction Manual for a 10 Year old where each line of the manual can be expanded into a logical line(s) of code.
+2. Try to offload logic out of the main section as much as possible by delegating to functions.
+
+Milestone 4: Code by expanding your 10 Year Olds "Instruction Manual
+1. Run your code snippets at every logical step to test correctness (Helps avoid debugging larger pieces of code later)
+2. Make sure you name the variables, functions clearly.
+3. Use libraries as much as possible
+
+Milestone 5: Prove that your code works using custom test cases
+1. Make sure you check boundary conditions and other test cases you noted in Milestone 1
+      a. If compiler is not available, dry run your code on a whiteboard or paper
+2. Suggest optimizations if applicable during interviews
+*/

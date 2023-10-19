d572fa01603cb1e2f603ca6170dd3a31f28deeba
taufik0747 <rahamantaufik1999@gmail.com>
2023-10-19 15:18:49 +0000

    Successfully submitted code for ME_PS_DS_CONCEPT_LINKED_LIST_NEW_MODULE_MOVEMIDDLETOHEAD

---
+++ ME_PS_DS_CONCEPT_LINKED_LIST_NEW/QUESTION_MoveMiddleToHead.java
@@ -0,0 +1,78 @@
+/*
+public class ListNode {
+   public int val; 
+   public ListNode next;
+   public ListNode(int x) { val = x; next = null; }
+}
+*/
+import java.util.*;
+import crio.ds.List.ListNode;
+
+class Solution{
+	
+	public ListNode moveMiddleToHead(ListNode head) {
+            if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // Find the middle node
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Break the list into two parts
+        prev.next = null;
+
+        // Find the last node of the first part
+        ListNode lastOfFirstPart = prev;
+
+        // Connect the last node of the first part to the head of the second part
+        lastOfFirstPart.next = slow.next;
+
+        // Update the head
+        ListNode newHead = slow;
+
+        // Connect the last node of the second part to the original head
+        slow.next = head;
+
+        return newHead;
+    
+	}
+}
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output
+
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving and see if you can recollect solving similar problems in the past
+      a. Obvious logic (this would only test ability to convert logic to code)
+      b. Figuring out logic
+      c. Knowledge of specific algorithm, data structure or pattern
+      d. Knowledge of specific domain or concepts
+      e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one based on the TC/SC requirements
+3. Get to a point where you can explain your approach to a 10 year old
+
+Milestone 3 : Come up with an Instruction Manual for a 10 year old
+1. Take a stab at the high-level logic & write it down like a detailed Instruction Manual for a 10 Year old where each line of the manual can be expanded into a logical line(s) of code.
+2. Try to offload logic out of the main section as much as possible by delegating to functions.
+
+Milestone 4: Code by expanding your 10 Year Olds "Instruction Manual
+1. Run your code snippets at every logical step to test correctness (Helps avoid debugging larger pieces of code later)
+2. Make sure you name the variables, functions clearly.
+3. Use libraries as much as possible
+
+Milestone 5: Prove that your code works using custom test cases
+1. Make sure you check boundary conditions and other test cases you noted in Milestone 1
+      a. If compiler is not available, dry run your code on a whiteboard or paper
+2. Suggest optimizations if applicable during interviews
+*/

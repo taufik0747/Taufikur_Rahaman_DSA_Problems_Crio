0fcc8f0bec7e089d77017fd7e0f16dab68c2c5bb
taufik0747 <rahamantaufik1999@gmail.com>
2023-12-19 23:50:41 +0000

    Successfully submitted code for ME_DSA_PRACTISE1_MODULE_VALIDANAGRAM

---
+++ ME_DSA_PRACTISE1/QUESTION_ValidAnagram.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class ValidAnagram {
+    public boolean validAnagram(String s, String t) {
+      if (s.length() != t.length()) {
+            return false;
+        }
+
+        // Array to keep track of character count
+        int[] count = new int[26];
+
+        // Increase count for characters in s and decrease for characters in t
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        // If count array has non-zero values, then s and t are not anagrams
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s = in.readLine();
+        String t = in.readLine();
+
+        boolean result = new ValidAnagram().validAnagram(s, t);
+        System.out.print(String.valueOf(result));
+    }
+}
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

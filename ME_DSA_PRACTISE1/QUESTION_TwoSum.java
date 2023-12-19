49503227058fa1664aabca8e438ac293a852e9e7
taufik0747 <rahamantaufik1999@gmail.com>
2023-12-19 23:37:25 +0000

    Successfully submitted code for ME_DSA_PRACTISE1_MODULE_TWOSUM

---
+++ ME_DSA_PRACTISE1/QUESTION_TwoSum.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+       int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+
+        return result; 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[scanner.nextInt()];
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = scanner.nextInt();
+        int target = scanner.nextInt();
+        scanner.close();
+
+        int[] complements = new TwoSum().twoSum(numbers, target);
+        System.out.print(complements[0] + " " + complements[1]);
+    }
+}
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */

4159953ab8c8458714e8c00e40e51832fc3d1892
taufik0747 <rahamantaufik1999@gmail.com>
2024-05-26 10:23:03 +0000

    Successfully submitted code for ME_PS_DS_CONCEPT_GRAPH_MODULE_TOPOLOGICALORDERING

---
+++ ME_PS_DS_CONCEPT_GRAPH/QUESTION_TopologicalOrdering.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class TopologicalOrdering {
+    private static List<Integer> topologicalOrdering(int n, List<List<Integer>> adj) {
+        int[] inDegree = new int[n + 1];
+        List<Integer> topoOrder = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Calculate in-degrees of all vertices
+        for (int u = 1; u <= n; u++) {
+            for (int v : adj.get(u)) {
+                inDegree[v]++;
+            }
+        }
+
+        // Add all vertices with in-degree 0 to the queue
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        // Process vertices in queue
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            topoOrder.add(u);
+            for (int v : adj.get(u)) {
+                inDegree[v]--;
+                if (inDegree[v] == 0) {
+                    queue.add(v);
+                }
+            }
+        }
+
+        return topoOrder;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int e = sc.nextInt();
+        
+        List<List<Integer>> adj = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < e; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+        
+        List<Integer> answer = topologicalOrdering(n, adj);
+        for (int elem : answer) {
+            System.out.print(elem + " ");
+        }
+        sc.close();
+    }
+}
+
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output
+
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving and see if you can recollect solving similar problems in the past
+      a. Obvious logic (this would only test ability to convert logic to code)
+      b. Figuring out logic
+      c. Knowledge of specific algorithm, data structure or pattern
+      d. Knowledge of specific domain or concepts
+      e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one based on the TC/SC requirements
+3. Get to a point where you can explain your approach to a 10 year old
+
+Milestone 3 : Come up with an Instruction Manual for a 10 year old
+1. Take a stab at the high-level logic & write it down like a detailed Instruction Manual for a 10 Year old where each line of the manual can be expanded into a logical line(s) of code.
+2. Try to offload logic out of the main section as much as possible by delegating to functions.
+
+Milestone 4: Code by expanding your 10 Year Olds "Instruction Manual
+1. Run your code snippets at every logical step to test correctness (Helps avoid debugging larger pieces of code later)
+2. Make sure you name the variables, functions clearly.
+3. Use libraries as much as possible
+
+Milestone 5: Prove that your code works using custom test cases
+1. Make sure you check boundary conditions and other test cases you noted in Milestone 1
+      a. If compiler is not available, dry run your code on a whiteboard or paper
+2. Suggest optimizations if applicable during interviews
+*/

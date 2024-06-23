aec96377cb883962b86912feb0605d9caa061e2e
taufik0747 <rahamantaufik1999@gmail.com>
2024-06-23 11:35:52 +0530

    Successfully submitted code for ME_DSA_LEVEL3_MOCK3_MODULE_MAXIMUMGAME

---
+++ ME_DSA_LEVEL3_MOCK3/QUESTION_MaximumGame.py
@@ -0,0 +1,59 @@
+from typing import List
+
+def maximumGame(n: int, nums: List[int]) -> int:
+    if n == 1:
+        return nums[0]
+    
+    dp = [0] * n
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    
+    for i in range(2, n):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+    
+    return dp[n-1]
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    n = int(data[0])
+    nums = list(map(int, data[1:n+1]))
+    print(maximumGame(n, nums))
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output
+
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving and see if you can recollect solving similar problems in the past
+      a. Obvious logic (this would only test ability to convert logic to code)
+      b. Figuring out logic
+      c. Knowledge of specific algorithm, data structure or pattern
+      d. Knowledge of specific domain or concepts
+      e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one based on the TC/SC requirements
+3. Get to a point where you can explain your approach to a 10 year old
+
+Milestone 3 : Come up with an Instruction Manual for a 10 year old
+1. Take a stab at the high-level logic & write it down like a detailed Instruction Manual for a 10 Year old where each line of the manual can be expanded into a logical line(s) of code.
+2. Try to offload logic out of the main section as much as possible by delegating to functions.
+
+Milestone 4: Code by expanding your 10 Year Olds "Instruction Manual
+1. Run your code snippets at every logical step to test correctness (Helps avoid debugging larger pieces of code later)
+2. Make sure you name the variables, functions clearly.
+3. Use libraries as much as possible
+
+Milestone 5: Prove that your code works using custom test cases
+1. Make sure you check boundary conditions and other test cases you noted in Milestone 1
+      a. If compiler is not available, dry run your code on a whiteboard or paper
+2. Suggest optimizations if applicable during interviews
+'''

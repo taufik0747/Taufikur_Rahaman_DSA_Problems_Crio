c59c6817c16332b8dbcda8cd4572aa375c7ed3af
taufik0747 <rahamantaufik1999@gmail.com>
2024-06-23 10:35:53 +0530

    Successfully submitted code for ME_DSA_LEVEL3_MOCK2_MODULE_PARKINGSPACE

---
+++ ME_DSA_LEVEL3_MOCK2/QUESTION_ParkingSpace.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class ParkingSpace {
+
+    public int parkingSpace(int[][] times) {
+        int n=times.length;
+        int[] arrivals=new int[n];
+        int[] departures= new int[n];
+        for(int i=0;i<n;i++){
+            arrivals[i]=times[i][0];
+            departures[i]=times[i][1];
+        }
+        Arrays.sort(arrivals);
+        Arrays.sort(departures);
+        int spaces=0;
+        int maxSpaces=0;
+        int i=0, j=0;
+        while(i<n && j<n){
+            if(arrivals[i]<departures[j]){
+                spaces++;
+                maxSpaces=Math.max(maxSpaces,spaces);
+                i++;
+
+            }else{
+                spaces--;
+                j++;
+            }
+        }
+        return maxSpaces;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[][] times = new int[n][2];
+
+        for(int i = 0 ; i < n ;i++) {
+            times[i][0] = scanner.nextInt();
+            times[i][1] = scanner.nextInt();
+        }
+
+        int result = new ParkingSpace().parkingSpace(times);
+
+        System.out.printf("%d", result);
+    }
+
+}
+
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output
+
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving and see if you can recollect solving similar problems in the past
+      a. Obvious logic (this would only test ability to convert logic to code)
+      b. Figuring out logic
+      c. Knowledge of specific algorithm, data structure or pattern
+      d. Knowledge of specific domain or concepts
+      e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one based on the TC/SC requirements
+3. Get to a point where you can explain your approach to a 10 year old
+
+Milestone 3 : Come up with an Instruction Manual for a 10 year old
+1. Take a stab at the high-level logic & write it down like a detailed Instruction Manual for a 10 Year old where each line of the manual can be expanded into a logical line(s) of code.
+2. Try to offload logic out of the main section as much as possible by delegating to functions.
+
+Milestone 4: Code by expanding your 10 Year Olds "Instruction Manual
+1. Run your code snippets at every logical step to test correctness (Helps avoid debugging larger pieces of code later)
+2. Make sure you name the variables, functions clearly.
+3. Use libraries as much as possible
+
+Milestone 5: Prove that your code works using custom test cases
+1. Make sure you check boundary conditions and other test cases you noted in Milestone 1
+      a. If compiler is not available, dry run your code on a whiteboard or paper
+2. Suggest optimizations if applicable during interviews
+*/
